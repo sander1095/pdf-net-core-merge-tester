84e0f3ed3e122066b4530ecad71294a57ce80143
Sander ten Brinke <s.tenbrinke@arcady.nl>
2021-03-22 12:32:09 +0100

    Image now appears on page

--- ITextPdfOSSCreator.cs
+++ ITextPdfOSSCreator.cs
@@ -1,5 +1,7 @@
-﻿using iTextSharp.text;
+﻿using iText.IO.Source;
+using iTextSharp.text;
 using iTextSharp.text.pdf;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using PdfReader = iTextSharp.text.pdf.PdfReader;
@@ -21,8 +23,8 @@ namespace pdfmerger
             //create newFileStream object which will be disposed at the end
             using var memoryStream = new MemoryStream();
             // step 2: we create a writer that listens to the document
-            PdfCopy copier = new PdfCopy(document, memoryStream);
-            if (copier == null)
+            PdfCopy copy = new PdfCopy(document, memoryStream);
+            if (copy == null)
             {
                 return null;
             }
@@ -32,48 +34,53 @@ namespace pdfmerger
 
             foreach (var blob in blobs)
             {
+                // https://stackoverflow.com/a/26111677/3013479
                 if (blob.ContentType.StartsWith("image/"))
                 {
-                    ReadImage(copier, blob.Content);
+                    ReadImage(copy, blob.Content);
                 }
                 else if (blob.ContentType.StartsWith("application/pdf"))
                 {
-                    ReadPdf(copier, blob.Content);
+                    ReadPdf(copy, blob.Content);
                 }
             }
 
             // step 5: we close the document and writer
-            copier.Close();
+            copy.Close();
             document.Close();
 
             return memoryStream;
 
-            static void ReadImage(PdfCopy copier, byte[] content)
+            static void ReadImage(PdfCopy copy, byte[] content)
             {
-                iTextSharp.text.Image pic = iTextSharp.text.Image.GetInstance(content);
+                // https://stackoverflow.com/a/26111677/3013479
 
-                if (pic.Height > pic.Width)
-                {
-                    //Maximum height is 800 pixels.
-                    float percentage = 0.0f;
-                    percentage = 700 / pic.Height;
-                    pic.ScalePercent(percentage * 100);
-                }
-                else
-                {
-                    //Maximum width is 600 pixels.
-                    float percentage = 0.0f;
-                    percentage = 540 / pic.Width;
-                    pic.ScalePercent(percentage * 100);
-                }
+                Document imageDocument = new Document();
+                ByteArrayOutputStream imageDocumentOutputStream = new ByteArrayOutputStream();
+                PdfWriter imageDocumentWriter = PdfWriter.GetInstance(imageDocument, imageDocumentOutputStream);
 
-                //pic.Border = iTextSharp.text.Rectangle.BOX;
-                //pic.BorderColor = iTextSharp.text.BaseColor.BLACK;
-                //pic.BorderWidth = 3f;
-                copier.Add(pic);
+                imageDocument.Open();
+                if (imageDocument.NewPage())
+                {
+                    var image = Image.GetInstance(content);
+
+                    if (!imageDocument.Add(image))
+                    {
+                        throw new Exception("Unable to add image to page!");
+                    }
+
+                    imageDocument.Close();
+                    imageDocumentWriter.Close();
+
+                    PdfReader imageDocumentReader = new PdfReader(imageDocumentOutputStream.ToArray());
+
+                    copy.AddPage(copy.GetImportedPage(imageDocumentReader, 1));
+
+                    imageDocumentReader.Close();
+                }
             }
 
-            static void ReadPdf(PdfCopy copier, byte[] content)
+            static void ReadPdf(PdfCopy copy, byte[] content)
             {
                 // we create a reader for a certain document
                 PdfReader reader = new PdfReader(content);
@@ -82,14 +89,14 @@ namespace pdfmerger
                 // step 4: we add content
                 for (int i = 1; i <= reader.NumberOfPages; i++)
                 {
-                    PdfImportedPage page = copier.GetImportedPage(reader, i);
-                    copier.AddPage(page);
+                    PdfImportedPage page = copy.GetImportedPage(reader, i);
+                    copy.AddPage(page);
                 }
 
                 PRAcroForm form = reader.AcroForm;
                 if (form != null)
                 {
-                    copier.CopyAcroForm(reader);
+                    copy.CopyAcroForm(reader);
                 }
 
                 reader.Close();
